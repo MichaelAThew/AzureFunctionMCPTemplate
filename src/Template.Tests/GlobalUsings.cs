aaa0de4fdcb081aaf3b4073495249f4427051c7c
KodachiQube <my_acc0unt5@pm.me>
2025-07-10 11:43:51 -0500

    Add Template.Tests project

---
+++ src/Template.Tests/GlobalUsings.cs
@@ -0,0 +1,6 @@
+global using Xunit;
+global using Moq;
+global using FluentAssertions;
+global using Microsoft.Extensions.Logging;
+global using Template.Core.Interfaces;
+global using Template.Core.Models;
